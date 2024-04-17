e465792200c555412a200565dfeac2b3d3d5d21f
Fahri Can <fahri-can@gmx.at>
2024-04-17 23:46:49 +0200

    fix: Adapt mappers to ignore certain fields.

--- src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListItemMapper.kt
+++ src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListItemMapper.kt
@@ -4,11 +4,13 @@ import com.udemy.groceryshoppingapi.dto.ShoppingListItemCreateRequest
 import com.udemy.groceryshoppingapi.dto.ShoppingListItemResponse
 import com.udemy.groceryshoppingapi.retail.entity.ShoppingListItem
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring", uses = [SupermarketMapper::class, GroceryItemMapper::class])
 interface ShoppingListItemMapper {
 
     fun toDto(entity: ShoppingListItem): ShoppingListItemResponse
 
+    @Mapping(target = "shoppingList", ignore = true)
     fun toEntity(dto: ShoppingListItemCreateRequest): ShoppingListItem
 }
--- src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListMapper.kt
+++ src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListMapper.kt
@@ -4,11 +4,14 @@ import com.udemy.groceryshoppingapi.dto.ShoppingListCreateRequest
 import com.udemy.groceryshoppingapi.dto.ShoppingListResponse
 import com.udemy.groceryshoppingapi.retail.entity.ShoppingList
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring", uses = [SupermarketMapper::class, ShoppingListItemMapper::class])
 interface ShoppingListMapper {
 
     fun toDto(entity: ShoppingList?): ShoppingListResponse
 
+    @Mapping(target = "appUser", ignore = true)
+    @Mapping(target = "done", ignore = true)
     fun toEntity(dto: ShoppingListCreateRequest): ShoppingList
 }
