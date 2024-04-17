aff61a4a8b5e304a82f1266a4a41194f50a0b881
Fahri Can <fahri-can@gmx.at>
2024-04-17 23:29:35 +0200

    fix: Adapt mapper and Open API specification.

--- src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListItemMapper.kt
+++ src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListItemMapper.kt
@@ -7,6 +7,7 @@ import org.mapstruct.Mapper
 
 @Mapper(componentModel = "spring", uses = [SupermarketMapper::class, GroceryItemMapper::class])
 interface ShoppingListItemMapper {
+
     fun toDto(entity: ShoppingListItem): ShoppingListItemResponse
 
     fun toEntity(dto: ShoppingListItemCreateRequest): ShoppingListItem
--- src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListMapper.kt
+++ src/main/kotlin/com/udemy/groceryshoppingapi/retail/util/ShoppingListMapper.kt
@@ -4,12 +4,11 @@ import com.udemy.groceryshoppingapi.dto.ShoppingListCreateRequest
 import com.udemy.groceryshoppingapi.dto.ShoppingListResponse
 import com.udemy.groceryshoppingapi.retail.entity.ShoppingList
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring", uses = [SupermarketMapper::class, ShoppingListItemMapper::class])
 interface ShoppingListMapper {
+
     fun toDto(entity: ShoppingList?): ShoppingListResponse
 
-    @Mapping(target = "shoppingListItems", source = "shoppingListItems")
     fun toEntity(dto: ShoppingListCreateRequest): ShoppingList
 }
