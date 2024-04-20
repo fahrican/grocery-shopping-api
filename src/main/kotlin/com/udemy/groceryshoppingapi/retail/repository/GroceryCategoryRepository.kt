84179ef40e02d7c4c1649261c4fbfb0c3b426daa
Fahri Can <fahri-can@gmx.at>
2024-04-20 16:02:37 +0200

    fix: Remove grocery category and put property in grocery item.

--- src/main/kotlin/com/udemy/groceryshoppingapi/retail/repository/GroceryCategoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.udemy.groceryshoppingapi.retail.repository
-
-import com.udemy.groceryshoppingapi.retail.entity.GroceryCategory
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface GroceryCategoryRepository: JpaRepository<GroceryCategory, Long> {
-}
