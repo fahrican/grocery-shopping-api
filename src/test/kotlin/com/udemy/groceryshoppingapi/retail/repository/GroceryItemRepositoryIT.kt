b4195669e2a83a08375442cb913a298c2ea3256c
Fahri Can <fahri-can@gmx.at>
2024-05-05 19:46:25 +0200

    chore: Add one more test for deletion of grocery item.

--- src/test/kotlin/com/udemy/groceryshoppingapi/retail/repository/GroceryItemRepositoryIT.kt
+++ src/test/kotlin/com/udemy/groceryshoppingapi/retail/repository/GroceryItemRepositoryIT.kt
@@ -2,7 +2,10 @@ package com.udemy.groceryshoppingapi.retail.repository
 
 import com.udemy.groceryshoppingapi.dto.Category
 import com.udemy.groceryshoppingapi.retail.entity.GroceryItem
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -18,12 +21,32 @@ class GroceryItemRepositoryIT @Autowired constructor(
 
     private val groceryItem = GroceryItem(name = "Apple", category = Category.FRUITS)
 
+    @BeforeEach
+    fun setUp() {
+        entityManager.persist(groceryItem)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        entityManager.clear()
+    }
+
     @Test
     fun `when grocery item gets persisted then check if it can be found after deleting it`() {
-        entityManager.persist(groceryItem)
         objectUnderTest.deleteGroceryItem(groceryItem.id)
 
         val item = objectUnderTest.findByIdOrNull(groceryItem.id)
         assertNull(item, "Item should be deleted")
+    }
+
+    @Test
+    fun `when grocery item gets persisted then check for an error`() {
+        val secondGroceryItem = GroceryItem(name = "Banana", category = Category.FRUITS)
+        entityManager.persist(secondGroceryItem)
+
+        objectUnderTest.deleteGroceryItem(secondGroceryItem.id)
+        val grocerItems: List<GroceryItem> = objectUnderTest.findAll()
+
+        assertEquals(1, grocerItems.size)
     }
 }
