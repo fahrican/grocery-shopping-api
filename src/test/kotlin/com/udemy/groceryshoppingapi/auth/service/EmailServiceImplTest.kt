eecb6628eedc8e7ec955b8804fcc27649629ac27
Fahri Can <fahri-can@gmx.at>
2024-05-05 15:43:01 +0200

    chore: Add tests for auth package.

---
+++ src/test/kotlin/com/udemy/groceryshoppingapi/auth/service/EmailServiceImplTest.kt
@@ -0,0 +1,82 @@
+package com.udemy.groceryshoppingapi.auth.service
+
+import com.udemy.groceryshoppingapi.error.SignUpException
+import com.udemy.groceryshoppingapi.user.entity.AppUser
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import jakarta.mail.MessagingException
+import jakarta.mail.internet.MimeMessage
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.mail.javamail.JavaMailSender
+
+
+internal class EmailServiceImplTest {
+
+    private val mockMailSender = mockk<JavaMailSender>(relaxed = true)
+
+    private val mockMimeMessage = mockk<MimeMessage>(relaxed = true)
+
+    private val objectUnderTest = EmailServiceImpl(mockMailSender)
+
+    private val dummyToken = "a12b34c56"
+
+    private val appUser = AppUser(
+        email = "test@aon.at",
+        appPassword = "password",
+        firstName = "Hamad",
+        lastName = "Al Khoury",
+        appUsername = "abu-ali"
+    )
+
+
+    @Test
+    fun `when send email verification is triggered then expect email successfully send`() {
+        every { mockMailSender.createMimeMessage() } returns mockMimeMessage
+        every { mockMailSender.send(any<MimeMessage>()) } returns Unit
+
+        objectUnderTest.sendVerificationEmail(appUser, dummyToken)
+
+        verify(exactly = 1) { mockMailSender.createMimeMessage() }
+        verify(exactly = 1) { mockMailSender.send(any<MimeMessage>()) }
+    }
+
+    @Test
+    fun `when send email verification is triggered then expect messaging exception`() {
+        every { mockMailSender.createMimeMessage() } throws SignUpException("failed to send email")
+
+        val actualException =
+            assertThrows<SignUpException> { objectUnderTest.sendVerificationEmail(appUser, dummyToken) }
+
+        assertEquals("failed to send email", actualException.message)
+        verify(exactly = 1) { mockMailSender.createMimeMessage() }
+        verify(exactly = 0) { mockMailSender.send(any<MimeMessage>()) }
+    }
+
+    @Test
+    fun `when send password reset email is triggered then expect password successfully reset`() {
+        val password = "test-password"
+        every { mockMailSender.createMimeMessage() } returns mockMimeMessage
+
+        objectUnderTest.sendPasswordResetEmail(appUser, password)
+
+        verify(exactly = 1) { mockMailSender.createMimeMessage() }
+        verify(exactly = 1) { mockMailSender.send(any<MimeMessage>()) }
+    }
+
+    @Test
+    fun `when send password reset email is triggered then expect messaging exception`() {
+        val password = "test-password"
+        every { mockMailSender.send(any<MimeMessage>()) } throws MessagingException("Simulated error")
+
+        val actualException = assertThrows<SignUpException> {
+            objectUnderTest.sendPasswordResetEmail(appUser, password)
+        }
+
+        assertEquals("failed to send email", actualException.message)
+        verify(exactly = 1) { mockMailSender.createMimeMessage() }
+        verify(exactly = 1) { mockMailSender.send(any<MimeMessage>()) }
+    }
+}
---
+++ src/test/kotlin/com/udemy/groceryshoppingapi/auth/web/rest/AuthenticationControllerIT.kt
@@ -0,0 +1,114 @@
+package com.udemy.groceryshoppingapi.auth.web.rest
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.udemy.groceryshoppingapi.auth.service.AccountManagementService
+import com.udemy.groceryshoppingapi.auth.service.JwtService
+import com.udemy.groceryshoppingapi.dto.AuthenticationRequest
+import com.udemy.groceryshoppingapi.dto.AuthenticationResponse
+import com.udemy.groceryshoppingapi.dto.EmailConfirmedResponse
+import com.udemy.groceryshoppingapi.dto.RegisterRequest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(controllers = [AuthenticationController::class])
+@AutoConfigureMockMvc(addFilters = false)
+internal class AuthenticationControllerIT {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var mockAccountManagementService: AccountManagementService
+
+    @MockBean
+    private lateinit var mockJwtService: JwtService
+
+    private val dummyEmailConfirmResponse = EmailConfirmedResponse("success response")
+
+    private val mapper = jacksonObjectMapper()
+
+
+    @Test
+    fun `when user sign up is triggered then expect success response`() {
+        val registerRequest = RegisterRequest(
+            firstName = "Mahmoud",
+            lastName = "Darwoush",
+            email = "mahmoud@aol.com",
+            username = "mahmoud-darwoush",
+            password = "123456",
+            passwordConfirmation = "123456"
+        )
+        `when`(mockAccountManagementService.signUp(registerRequest)).thenReturn(dummyEmailConfirmResponse)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(registerRequest))
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isCreated)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.message").value(dummyEmailConfirmResponse.message))
+    }
+
+    @Test
+    fun `when user verify is triggered then expect success response`() {
+        val token = "a1b2c3"
+        `when`(mockAccountManagementService.verifyUser(token)).thenReturn(dummyEmailConfirmResponse)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/auth/verify")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("token", token)
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.message").value(dummyEmailConfirmResponse.message))
+    }
+
+    @Test
+    fun `when user sign in triggered then expect success response`() {
+        val request = AuthenticationRequest(username = "mahmoud-darwoush", password = "123456")
+        val response = AuthenticationResponse(accessToken = "acess123", refreshToken = "refresh123")
+        `when`(mockAccountManagementService.signIn(request)).thenReturn(response)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/sign-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request))
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.accessToken").value(response.accessToken))
+    }
+
+    @Test
+    fun `when user password reset triggered then expect success response`() {
+        val email = "mahmoud@aol.com"
+        `when`(mockAccountManagementService.requestPasswordReset(email)).thenReturn(dummyEmailConfirmResponse)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/password-reset")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("email", email)
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.message").value(dummyEmailConfirmResponse.message))
+    }
+}
