157e23de23b7734affa72311df109380eb5a71db
Fahri Can <fahri-can@gmx.at>
2024-05-18 21:37:17 +0200

    chore: Finish with integration tests for GroceryItemController.

--- src/test/kotlin/com/udemy/groceryshoppingapi/retail/web/rest/GroceryItemControllerIT.kt
+++ src/test/kotlin/com/udemy/groceryshoppingapi/retail/web/rest/GroceryItemControllerIT.kt
@@ -2,33 +2,41 @@ package com.udemy.groceryshoppingapi.retail.web.rest
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.udemy.groceryshoppingapi.auth.service.ClientSessionService
+import com.udemy.groceryshoppingapi.auth.service.JwtService
 import com.udemy.groceryshoppingapi.dto.Category
 import com.udemy.groceryshoppingapi.dto.GroceryItemResponse
+import com.udemy.groceryshoppingapi.dto.GroceryItemUpdateRequest
 import com.udemy.groceryshoppingapi.retail.service.ShoppingListService
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
 @AutoConfigureMockMvc(addFilters = false)
-class GroceryItemControllerIT {
+@WebMvcTest(controllers = [GroceryItemController::class])
+internal class GroceryItemControllerIT {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @MockBean
+    private lateinit var mockJwtService: JwtService
+
+    @MockBean
     private lateinit var mockUserProvider: ClientSessionService
 
     @MockBean
-    private lateinit var mockShoppingListService: ShoppingListService
+    private lateinit var mockService: ShoppingListService
 
     private val mapper = jacksonObjectMapper()
 
@@ -39,7 +47,7 @@ class GroceryItemControllerIT {
     @Test
     fun `when get grocery item called then expect 200 status`() {
         Mockito.`when`(
-            mockShoppingListService.getGroceryItem(
+            mockService.getGroceryItem(
                 listId,
                 itemId,
                 mockUserProvider.getAuthenticatedUser()
@@ -58,4 +66,23 @@ class GroceryItemControllerIT {
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.category").value(response.category.toString()))
     }
 
+    @Test
+    fun `when update grocery item called then expect 200 status`() {
+        val request = GroceryItemUpdateRequest(name = "Hummus", category = Category.OTHER)
+        Mockito.`when`(
+            mockService.updateGroceryItem(1, request)
+        ).thenReturn(response)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.patch("/api/v1/shopping-lists/$listId/shopping-list-items/$itemId/grocery-item/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request))
+        )
+
+        resultActions.andExpect((MockMvcResultMatchers.status().isOk))
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(response.id))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.name").value(response.name))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.category").value(response.category.toString()))
+    }
 }
