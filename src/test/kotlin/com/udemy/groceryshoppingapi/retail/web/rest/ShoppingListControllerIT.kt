052b9de55c8358866e7d4748b3a3b39a8558a113
Fahri Can <fahri-can@gmx.at>
2024-05-20 00:44:53 +0200

    chore: Finish to write integration tests for controller.

--- src/test/kotlin/com/udemy/groceryshoppingapi/retail/web/rest/ShoppingListControllerIT.kt
+++ src/test/kotlin/com/udemy/groceryshoppingapi/retail/web/rest/ShoppingListControllerIT.kt
@@ -5,8 +5,10 @@ import com.udemy.groceryshoppingapi.auth.service.ClientSessionService
 import com.udemy.groceryshoppingapi.dto.Hypermarket
 import com.udemy.groceryshoppingapi.dto.ShoppingListCreateRequest
 import com.udemy.groceryshoppingapi.dto.ShoppingListResponse
+import com.udemy.groceryshoppingapi.dto.ShoppingListUpdateRequest
 import com.udemy.groceryshoppingapi.dto.SupermarketCreateRequest
 import com.udemy.groceryshoppingapi.dto.SupermarketResponse
+import com.udemy.groceryshoppingapi.dto.SupermarketUpdateRequest
 import com.udemy.groceryshoppingapi.retail.service.ShoppingListService
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
@@ -45,9 +47,10 @@ class ShoppingListControllerIT {
         shoppingListItems = emptyList(),
         totalAmount = 0.00F
     )
+    private val listId: Long = 99
 
     @Test
-    fun `when create shopping list then expect 200 status`() {
+    fun `when create shopping list then expect 201 status`() {
         val createReq = ShoppingListCreateRequest(
             receiptPictureUrl = "https://xyz.com/list.jpg",
             supermarket = SupermarketCreateRequest(Hypermarket.HOFER),
@@ -64,6 +67,79 @@ class ShoppingListControllerIT {
         )
 
         result.andExpect((status().isCreated))
+        result.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(response.id))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.isDone").value(response.isDone))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.receiptPictureUrl").value(response.receiptPictureUrl))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.totalAmount").value(response.totalAmount))
+    }
+
+    @Test
+    fun `when delete shopping list is called then expect 204 status`() {
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.delete("/api/v1/shopping-lists/$listId")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+
+        resultActions.andExpect((status().isNoContent))
+    }
+
+    @Test
+    fun `when get shopping list is called then expect 200 status`() {
+        Mockito.`when`(
+            mockService.getShoppingListById(listId, mockUserProvider.getAuthenticatedUser())
+        ).thenReturn(response)
+
+        val result: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/shopping-lists/$listId")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+
+        result.andExpect((status().isOk))
+        result.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(response.id))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.isDone").value(response.isDone))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.receiptPictureUrl").value(response.receiptPictureUrl))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.totalAmount").value(response.totalAmount))
+    }
+
+    @Test
+    fun `when get shopping lists is called then expect 200 status`() {
+        Mockito.`when`(
+            mockService.getShoppingLists(mockUserProvider.getAuthenticatedUser(), null)
+        ).thenReturn(setOf(response))
+
+        val result: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/shopping-lists")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+
+        result.andExpect((status().isOk))
+        result.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(response.id))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$[0].isDone").value(response.isDone))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$[0].receiptPictureUrl").value(response.receiptPictureUrl))
+        result.andExpect(MockMvcResultMatchers.jsonPath("$[0].totalAmount").value(response.totalAmount))
+    }
+
+    @Test
+    fun `when update shopping list is called then expect 200 status`() {
+        val updateReq = ShoppingListUpdateRequest(
+            receiptPictureUrl = "https://xyz.com/list.jpg",
+            supermarket = SupermarketUpdateRequest(Hypermarket.HOFER),
+            isDone = false
+        )
+        Mockito.`when`(
+            mockService.updateShoppingList(listId, updateReq, mockUserProvider.getAuthenticatedUser())
+        ).thenReturn(response)
+
+        val result: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.patch("/api/v1/shopping-lists/$listId")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateReq))
+        )
+
+        result.andExpect((status().isOk))
         result.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
         result.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(response.id))
         result.andExpect(MockMvcResultMatchers.jsonPath("$.isDone").value(response.isDone))
